c5e5c8a0425814487c18d739ca2ddd8bdf9f864a
Aprilia Ramadhayanti <aprilia@mdcorp.com>
2026-01-24 11:54:39 +0700

    fixing auth

--- server/api/user.get.ts
+++ server/api/user.get.ts
@@ -1,5 +1,7 @@
 export default defineEventHandler((event) => {
   // Pastikan return value selalu terdefinisi, jangan undefined
   // Jika user null (belum login), kembalikan null
-  return event.context.user || null;
+  return {
+    user: event.context.user || null
+  };
 });
